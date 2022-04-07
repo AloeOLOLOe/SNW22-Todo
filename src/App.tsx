bb81c54a757665f80d2e33201d4eae3694101dd8
AloeOLOLOe <st.seregin@gmail.com>
2022-04-07 12:02:22 +0300

    m1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+    const arr1 = [
+        {id: 1, title: "HTML&CSS1", isDone: true},
+        {id: 2, title: "JS1", isDone: true},
+        {id: 3, title: "ReactJS1", isDone: false},
+        {id: 4, title: "Rest API1", isDone: false},
+            ]
+    const arr2 = [
+        {id: 1, title: "HTML&CSS2", isDone: true},
+        {id: 2, title: "JS2", isDone: true},
+        {id: 3, title: "ReactJS2", isDone: false},
+        {id: 4, title: "Rest API2", isDone: false},
+        {id: 5, title: "GraphQL2", isDone: false}
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist topic1={'What to learn-1111'} arr={arr1}/>
+            <Todolist topic1={'What to learn-2222'} arr={arr2}/>
         </div>
     );
 }
